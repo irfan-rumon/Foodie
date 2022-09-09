11ef70752b7d66765d10a1ad58ef50f36d7c8e33
irfan-rumon <irfannisho8571@gmail.com>
2022-09-09 23:47:54 +0600

    Add feature of adding quantity of a food on cart

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -11,17 +11,30 @@ import { Router } from '@angular/router';
 export class CartComponent implements OnInit {
 
   products: Product[] = []; 
+  subtotal:number = 0;
+  shipping:number = 0;
+  total:number = 0;
 
   constructor(private cartApi: CartapiService, private router:Router) { }
 
   ngOnInit(): void {
     this.cartApi.getProducts().subscribe(  (products) => {this.products = products} );
-    console.log(this.products);
+    if( this.products.length > 0){
+      this.shipping = 50;
+      
+    }
   }
 
   onProductDelete(product:Product){
      this.cartApi.deleteProduct(product).subscribe();
      window.location.reload();
+  }
+
+  onAddQuantity(product:Product){
+       product.quantity++;
+       product.totalPrice += product.unitPrice;
+       this.subtotal += product.unitPrice;
+       this.total += product.unitPrice;
   }
 
 }
