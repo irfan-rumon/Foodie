47f265038623558d77b3a453930269657d675411
irfan-rumon <irfannisho8571@gmail.com>
2022-09-13 20:12:41 +0600

    Fix: Page reloading while interaction with cart componenents

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -4,6 +4,7 @@ import { CartapiService } from '../cartapi.service';
 import { Router } from '@angular/router';
 import { Payment } from '../payment';
 import { PaymentInfoService } from '../payment-info.service';
+import { ProductapiService } from '../productapi.service';
 
 @Component({
   selector: 'app-cart',
@@ -13,20 +14,18 @@ import { PaymentInfoService } from '../payment-info.service';
 export class CartComponent implements OnInit {
 
   cartProducts: Product[] = []; 
-  numOfCartProducts: number;
   payment: Payment = {} as Payment;
   
 
   constructor(private cartApi: CartapiService, private paymentApi: PaymentInfoService, private router:Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { //main chalenging part, somehow load the data
     this.cartApi.getProducts().subscribe(  (products) => {
       this.cartProducts = products;
     } );
     this.paymentApi.getPaymentInfo().subscribe(  (payment)=>{
         this.payment = payment;
     } )
-    this.numOfCartProducts = this.cartProducts.length;
   }
 
 
@@ -40,65 +39,39 @@ export class CartComponent implements OnInit {
          total: updatedTotal
       }
 
-      this.cartApi.deleteProduct(product).subscribe();
-      this.paymentApi.editPaymentInfo(updatedPayment).subscribe();
-      window.location.reload();
+      this.cartApi.deleteProduct(product).subscribe(  () => (this.cartProducts = this.cartProducts.filter((p) => p.id !== product.id)) );//internal array thekei delete
+      this.payment.subtotal -= product.totalPrice;
+      this.payment.total -= product.totalPrice;
+      if( this.cartProducts.length == 0)this.payment.shipping = 0;
+    
   }
 
   onAddQuantity(product:Product){
-      let updatedProduct:Product = {
-        id: product.id,
-        imageURL: product.imageURL,
-        name: product.name,
-        unitPrice : product.unitPrice,
-        quantity: product.quantity + 1,
-        totalPrice : product.totalPrice + product.unitPrice,
-        addedToCart: true
-      }
-
-     
-      let updatedSubTotal:number = this.payment.subtotal + product.unitPrice;
-      let updatedTotal:number = this.payment.total + product.unitPrice ;
-      let updatedPayment:Payment = {
-         subtotal : updatedSubTotal,
-         shipping: 50,
-         total: updatedTotal
-      }
-
-      this.cartApi.editProduct(product.id, updatedProduct).subscribe();
-      this.paymentApi.editPaymentInfo(updatedPayment).subscribe();
-      window.location.reload();
       
+      let foundIndex = this.cartProducts.findIndex(x => x.id == product.id);
+      this.cartProducts[foundIndex].quantity++; //internal array change
+      this.cartProducts[foundIndex].totalPrice += this.cartProducts[foundIndex].unitPrice;
+
+      this.payment.subtotal += product.unitPrice;
+      this.payment.total += product.unitPrice;
+      this.payment.shipping = 50;
   }
 
   onMinusQuantity(product:Product){
-   
+
     if( product.quantity == 1){
       this.onProductDelete(product);
       return;
     }
-    let updatedProduct:Product = {
-      id: product.id,
-      imageURL: product.imageURL,
-      name: product.name,
-      unitPrice : product.unitPrice,
-      quantity: product.quantity - 1,
-      totalPrice : product.totalPrice - product.unitPrice,
-      addedToCart : true
-    }
+   
+    let foundIndex = this.cartProducts.findIndex(x => x.id == product.id);
+      this.cartProducts[foundIndex].quantity--; //internal array change
+      this.cartProducts[foundIndex].totalPrice -= this.cartProducts[foundIndex].unitPrice;
 
-    let updatedSubTotal = this.payment.subtotal - product.unitPrice;
-    let updatedTotal = this.payment.total - product.unitPrice ;
-    let updatedPayment:Payment = {
-       subtotal : updatedSubTotal,
-       shipping: 50,
-       total: updatedTotal
-    }
-
-    this.cartApi.editProduct(product.id, updatedProduct).subscribe();
-    this.paymentApi.editPaymentInfo(updatedPayment).subscribe();
-    
-    window.location.reload();
+      this.payment.subtotal -= product.unitPrice;
+      this.payment.total -= product.unitPrice;
+      if( this.cartProducts.length == 0)this.payment.shipping = 0;
+      else this.payment.shipping = 50;
   }
 
 }
