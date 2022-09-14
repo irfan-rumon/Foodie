ea4b0dedb23f44894e8c2ad3cc6db2b5b78a9c08
irfan-rumon <irfannisho8571@gmail.com>
2022-09-14 23:09:43 +0600

    Refactor: Design of admin panel

--- frontend/src/app/admin/admin-routing.module.ts
+++ frontend/src/app/admin/admin-routing.module.ts
@@ -10,8 +10,8 @@ import { OrderComponent } from './order/order.component';
 const routes: Routes = [
    
     //canActivate: [AuthGuard],
-      {path : '', component: AdminDashboardComponent},
-      {  path: 'products', component: AdminProductsComponent},
+      {path : '', component: AdminProductsComponent},
+      { path: 'products', component: AdminProductsComponent},
       { path: 'products/add', component: AddProductsComponent},
       { path: 'products/:id/edit', component: EditProductComponent},
       { path: 'orders', component: OrderComponent}
